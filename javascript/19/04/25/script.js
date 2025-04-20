56bdf8cba357d5c88fec45634c2d0a13da796519
sayali singh <sayalibaghel6@gmail.com>
2025-04-20 21:03:24 +0530

    n

---
+++ javascript/19/04/25/script.js
@@ -0,0 +1,19 @@
+// array
+const num=[1,2,3,4,5];
+    console.log(num ,typeof num);
+
+    // object
+    const user ={name:"sayali",age:22};
+    console.log(user,typeof user);
+    // function declaration
+    function name(){
+console.log("hello");
+    }
+    // fuction calling
+    name();
+    console.log(name, typeof name)
+
+    // concatination
+    let x ;
+    x= "hii" + " "+"world";
+    console.log(x);
---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+//  const fruits = [ "bnana", "mango"," grapes"]
+
+//     for(var a of fruits) {
+//         console.log(a)
+//     } 
+
+
+// const num = { num:'say' }
+
+// for( const key in num){
+//     console.log(key ,num[key])
+// }
+
+
+
+//   image('say')
+
+// for( const a of fruits){
+//     console.log(a)
+// }
+
+
+//  const bigFruit = fruits.filter((fruit) =>{
+//     console.log(fruit)
+//     return fruit.includes('m')
+    
+// const num = [ 2,2,15]
+
+// num.reduce( (num,current)=>{
+
+//    console.log(num)
+//    return 8},0)
+
+// //  
+// debugger
+//  for( a of num){
+//    console.log (a)
+//  }
+
+//  const result = num.some( (a,i) => {
+//    if( a % 2 == 1){
+//       console.log(i)
+//    }
+//    return a % 2 === 1
+// })
+
+// const user = ["hii","hee"]
+
+// const fruit = users.filter( (user) => {
+   
+//    return user.length >=4
+// })
