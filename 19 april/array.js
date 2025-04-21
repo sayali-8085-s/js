8a6945267f5d0d37857db465d26f1ea52c09a721
sayali singh <sayalibaghel6@gmail.com>
2025-04-21 19:03:45 +0530

    n

--- 19 april/array.js
+++ 19 april/array.js
@@ -1,4 +1,4 @@
-// array
+array
 const num=[1,2,3,4,5];
     console.log(num ,typeof num);
 
@@ -17,3 +17,5 @@ console.log("hello");
     let x ;
     x= "hii" + " "+"world";
     console.log(x);
+
+
---
+++ 21 april/string.js
@@ -0,0 +1,31 @@
+// string to number conversion
+var a ="5";
+a = parseFloat(a);
+console.log( a ,typeof a);
+
+
+// number to string
+var b =45;
+b = String(b);
+console.log(b , typeof b);
+
+// to boolean
+b = Boolean(b);
+console.log(b,typeof b);
+
+
+// template literal or template string or backticks
+
+var text = `my age is ${a}`;
+// property of string
+// 1) length
+var x ="helllo world";
+
+console.log(x.length);
+// chart at
+console.log(x.charAt(x));
+
+// include
+// replace we give 
+// substring or slice
+console.log(x.substring(0,3))
