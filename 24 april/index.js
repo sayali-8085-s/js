7307df33c953d029f7fb83f58a32b18f4a854e45
sayali singh <sayalibaghel6@gmail.com>
2025-04-24 16:26:57 +0530

    n

---
+++ 24 april/index.js
@@ -0,0 +1,47 @@
+// var a =  parseFloat(prompt("enter a number"));
+// var b =  parseFloat(prompt("enter a number"));
+// console.log(a + b);
+
+
+// var a = parseFloat(prompt("enter "));
+// var b = Math.PI;
+// console.log(b * a *a);
+
+
+// var a = parseFloat(prompt("enter "));
+// if (a % 2 ==0){
+//     console.log("even")
+// }
+
+
+// var a = parseFloat(prompt("enter"));
+// if ( a> 0){
+//     console.log("positive");
+// }
+// else{
+//     console.log("negative")
+// }
+
+// var a = parseFloat(prompt("enter"));
+// var b = parseFloat(prompt("enter"));
+
+// if ( a % 2 ==0){
+//     console.log(a*b);
+// }
+
+// else
+// {
+// console.log(1/2 * a *b);
+// }
+
+// var a = parseFloat(prompt("enter"));
+// var i ;
+// for( i = 1 ; i<=10 ; i++){
+//     console.log( a*i);
+// }
+
+var a = parseFloat(prompt("enter"));
+if (a % 2 !=0){
+       console.log("odd")
+}
+// reverse of number otherwise even fint sqaure then table
