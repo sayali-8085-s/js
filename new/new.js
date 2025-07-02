e911e8106b6acc91c23e38349422d04679f10ce6
sayali singh <sayalibaghel6@gmail.com>
2025-07-02 16:29:54 +0530

    n

---
+++ new/new.js
@@ -0,0 +1,40 @@
+// let num1 = parseInt(prompt("enter a value"))
+// // let num2 = parseInt(prompt("enter a value"))
+// console.log(3.14*num1*num1)
+
+
+// let num1 = parseInt(prompt("enter a value"));
+// let num2 = parseInt(prompt("enter a value"));
+// let num3 ;
+
+// num3 = num1
+// num1 = num2
+// num2 = num3
+
+// console.log(num1)
+// console.log(num2)
+
+
+// let n = parseInt(prompt("enter a value"));
+
+// if(n>0){
+//     console.log('positive')
+// }
+
+
+// else{
+//     console.log('negative')
+// }
+
+
+let n = parseInt(prompt("enter a value"));
+let m = parseInt(prompt("enter a value"));
+
+if(m%2==0){
+        console.log(m)
+ }
+
+
+ else{
+    console.log(n*m)
+ }
