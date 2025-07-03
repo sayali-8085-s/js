0a8346bfdb9eeb9ae18fed8ed505cb32f92317cb
sayali singh <sayalibaghel6@gmail.com>
2025-07-03 16:30:19 +0530

    n

--- new/new.js
+++ new/new.js
@@ -27,14 +27,79 @@
 // }
 
 
-let n = parseInt(prompt("enter a value"));
-let m = parseInt(prompt("enter a value"));
+// let n = parseInt(prompt("enter a value"));
+// let m = parseInt(prompt("enter a value"));
 
-if(m%2==0){
-        console.log(m)
- }
+// if(m%2==0){
+//         console.log(m)
+//  }
 
 
- else{
-    console.log(n*m)
- }
+//  else{
+//     console.log(n*m)
+//  }
+
+
+
+// lader if else - multiple condition
+// let a = parseInt(prompt("enter a value"));
+// let b= parseInt(prompt("enter a value"));
+// let c = parseInt(prompt("enter a value"));
+// let d =( a+b+c)
+// let e = (d /300)*100
+// if(e>=70){
+//    console.log('a grade')
+
+// }
+
+// else if (e>=50){
+//    console.log('b grade')
+
+// }
+
+// else{
+//    console.log('c grade')
+
+// }
+
+
+// nested 
+// let a = parseInt(prompt("enter a age"));
+
+
+
+// if(a>=18){
+//    let b = parseInt(prompt("enter a 1 indain"));
+//    if(b==1){
+//       console.log('eligible')
+//    }
+//    else{
+//       console.log('not eligible')
+//    }
+
+// }
+
+// else{
+//    console.log('minor')
+// }
+
+
+
+// loop
+// do while ,while, for of ,for in ,forEach
+
+//  let num =10 
+//  let b = parseInt(prompt("enter a 1 indain"));
+//  do {
+//    console.log(b*num);
+//    num--
+   
+//  }
+
+//  while(num>=1);
+  
+
+
+while(){
+   
+}
