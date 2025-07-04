17b96844f888e2371f092f4636308c1b113575bf
sayali singh <sayalibaghel6@gmail.com>
2025-07-04 16:27:41 +0530

    n

--- new/new.js
+++ new/new.js
@@ -99,7 +99,39 @@
 //  while(num>=1);
   
 
+//  for(let a=1 ; a<=20; a++){
+//      if( a%2==0){
+//         console.log(a*a)
+//  }
 
-while(){
-   
+
+// }
+
+
+// for( let a=2 ; a<=3 ;a++){
+//     for( let b=1 ; b<=10 ; b++){
+//         console.log(a*b);
+//     }
+// }
+
+
+
+// for(let a=1 ; a<=5;a++){
+//     for(let b=1 ; b<=5;b++ ){
+//        document.write("*")
+//     }
+//      document.write("<br>")
+    
+// }
+
+for(let a =1 ; a<=5 ;a++){
+    for( let b = 1 ; a<=5 ;b++){
+        if(a==1 || a==5 || b==1 || b==5){
+            document.write("*")
+        }
+        else{
+            document.write('$nbsp$nbsp')
+        }
+    }
+    document.write("<br>")
 }
