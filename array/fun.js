1f68b379d99ae1e7a526942cbb1f2b2df244033b
sayali singh <sayalibaghel6@gmail.com>
2025-07-07 16:31:56 +0530

    new

--- array/fun.js
+++ array/fun.js
@@ -19,13 +19,104 @@ console.log(list)
 
 
 
-let sum =0
-var num =  [ 1,2 ,3 ,4,5,6,7,8,9,10]
+// let sum =0
+// var num =  [ 1,2 ,3 ,4,5,6,7,8,9,10]
 
-for(let a = 0 ;a < num.length; a++ ){
-      if( num[a]%2 !=0){
-         sum =sum+num[a]
-    }
-}
-console.log(sum)
+// for(let a = 0 ;a < num.length; a++ ){
+//       if( num[a]%2 !=0){
+//          sum =sum+num[a]
+//     }
+// }
+// console.log(sum)
 
+
+
+// FOR OF LOOP - to fetch the value from array without indexing
+
+// var num =  [ 1,2 ,3 ,4,5,6,7,8,9,10]
+
+// for(let v of num){
+//     console.log(v)
+// }
+
+
+
+// FOR IN LOOP - GETor fetch  INDEXING OF ARRAY ELEMAENT
+
+// var num =  [ 1,2 ,3 ,4,5,6,7,11,9,10]
+
+// for(let v in num){
+//     console.log(v)
+// }
+
+
+
+
+// let sum =0
+
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+// for(let v of num ){
+     
+//          sum =sum+v
+    
+// }
+// console.log(sum)
+
+
+// METHOD
+
+// push - to add multiple element in last 
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+// num.push('werwt','say')
+// console.log(num)
+
+
+
+// // pop - 
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+// num.pop()
+// console.log(num)
+
+
+// // unsift
+
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+// num.unshift('say')
+// console.log(num)
+
+
+// // shift
+
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+// num.shift()
+// console.log(num)
+
+
+
+// slice
+
+// var num =  [ 11,12,13,14,15,16,17,18,19,20]
+//  let s =num.slice(1,4)
+// console.log(s)
+
+
+
+// let a =[1,2,3,4,5,6,7,8,9,10]
+
+// let s =a.slice(3,9)
+// for(let v of s ){
+
+//     if(v%2==0){
+//         console.log(v)
+//     }
+
+
+// }
+
+
+
+// SPLICE - remove add (index of start,   of element to delete,   elemt to add )
+
+var num =  [ 11,12,13,14,15,16,17,18,19,20]
+ num.splice(2 ,1 ,20  )
+console.log(num)
