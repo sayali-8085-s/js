9d5b39685fa37e9982ebabb4359e20454c3e3c60
sayali singh <sayalibaghel6@gmail.com>
2025-07-16 19:45:57 +0530

    new

--- dom/index.js
+++ dom/index.js
@@ -14,29 +14,59 @@
 // }
 
 
+// function first() {
+//      let a= document.getElementById("first")
+//      a.style.backgroundImage =" URL('C:\Users\DELL\OneDrive\Desktop\js\dom\index.css')"
+//      a.style.backgroundSize = "cover"
+    
+// }
+
+// function textcolor1(){
+//     let a= document.getElementById("first")
+//      a.style.backgroundColor='red'
+    
+
+// }
+
+
+// function second() {
+//      let b= document.getElementById("second")
+//      b.style.backgroundColor='green'
+    
+// }
+
+// function textcolor2(){
+//     let b= document.getElementById("second")
+//      b.style.backgroundColor='green'
+    
+
+// }
+
+
 function first() {
-     let a= document.getElementById("first")
-     a.style.backgroundColor='red'
-    
+     let a= document.getElementById("main")
+      a.style.backgroundImage ="url('dog.webp')"
+   
 }
 
-function textcolor1(){
-    let a= document.getElementById("first")
-     a.style.backgroundColor='red'
-    
+function first1() {
+     let a= document.getElementById("main")
+      a.style.backgroundImage ="url('download.jpeg')"
+   
+}
 
+function first2() {
+     let a= document.getElementById("main")
+      a.style.backgroundImage ="url('dog.webp')"
+   
 }
 
 
-function second() {
-     let b= document.getElementById("second")
-     b.style.backgroundColor='green'
-    
-}
 
-function textcolor2(){
-    let b= document.getElementById("second")
-     b.style.backgroundColor='green'
-    
+
+
+function sa(){
+      let a= document.getElementById("cmg")
+      a.src="dog.webp"
 
 }
